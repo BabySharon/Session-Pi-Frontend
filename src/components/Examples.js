6ca3fa0c663d33d9f631a812f6c8127f9f4c253d
sharon baby <sharonoorakathu99@gmail.com>
2022-09-02 15:07:18 +0100

    Changes

--- src/components/Examples.js
+++ src/components/Examples.js
@@ -1,7 +1,23 @@
-import React, { Component }  from 'react';
+import React, { Component, useEffect, useState }  from 'react';
 function Examples(){
+    const [input, setInput] = useState();
+    const [p, setP] = useState();
+    const[q, setQ] = useState();
+
+    useEffect(() => {setInput("new x y  P[x branch {l1:x<m>, l2:x(2)}.zero] | Q[ y select l2.(y<a>).zero]");
+    setP("Session type of P: &{l1:!Int, l2:?Int}.end\t Typint context: m:Int");
+    setQ("Session type of Q: +{l1:?Int, l2:!Int}.end\t Typing context: a:Int")
+
+}, [])
     return(
-        <h1>English</h1>
+        <>
+        <h1>Examples and resources</h1>
+        <a href="http://www.dcs.gla.ac.uk/~ornela/publications/DGS17.pdf"> Click here to read on session types</a>
+        <p>An example to try:</p>
+        <p>Input: {input}</p> 
+        <p>: {p}</p>
+        <p>: {q}</p>
+        </>
     );
 }
 export default Examples;
--- src/components/UserGuide.js
+++ src/components/UserGuide.js
@@ -2,13 +2,19 @@ import React, { Component, useEffect, useState }  from 'react';
 
 function UserGuide(){
     const [eg, setExample] = useState( );
+    const [type, setTyper] = useState();
 
-    useEffect(()=>{setExample("new x y  P[x branch {l1:x<m>, l2:x(2)}.zero] | Q[ y select l2.(y<a>).zero]")}, [])
+    useEffect(()=>{setExample("new x y  P[x branch {l1:x<m>, l2:x(2)}.zero] | Q[ y select l2.(y<a>).zero]");
+setTyper("+{l1 :?Int, l2 :!Int∧?Bool}")}, [])
     return (
         <>
         <h1>User Guide</h1>
         <ol>
             <li>Enter the names of the processes in square brackets along with the input. Eg: {eg}</li>
+            <li>Click on the Plus sign to add more process descriptions</li>
+            <li>Click on the cross button to delete process description</li>
+            <li>Enter the typing contexts like a:Int</li>
+            <li>The session type's syntax for branching and select is </li>
         </ol>
         </>
     );
